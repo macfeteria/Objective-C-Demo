45fb6669fec8c6a58eb3ee33fecf39ab260121d1
ter_ <macfeteria@gmail.com>
2013-08-31 15:39:17 +0200

    Initial Commit

---
+++ Chapter 16/Program 16.1/Program 16.1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Program 16.1
+//
+//  Created by Ter on 8/31/13.
+//  Copyright (c) 2013 Ter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
